91974cd6f54809410b437b7705184ca6bb186cad
JustasTomkus <justas.tomkus@google.com>
2011-04-16 16:30:58 +0300

    + physics.... whee

--- game/Shop.lua
+++ game/Shop.lua
@@ -2,19 +2,27 @@ Shop = {}
 
 function Shop.load()
 	Shop.image = love.graphics.newImage(pre.."Shop.png")
+	Shop.w = 64
+	Shop.h = 64
 	Shop.List = {}
     Shop.AddShop(300,150, 1)
     Shop.AddShop(150,300, 2)
 end
 
 function Shop.AddShop(xx, yy, weaponid)
+	local phyStmp = {}
+    phyStmp.b = love.physics.newBody(world, xx, yy, 1,0)
+    phyStmp.s = love.physics.newRectangleShape(phyStmp.b, 0, 0, Shop.w, Shop.h, 0)
+    phyStmp.s:setData("Shop"..weaponid)
+	phyStmp.s:setSensor(true)
 	table.insert(Shop.List, {
 		image = Shop.image,
 		x = xx,
 		y = yy,
 		vx = 60,
 		vy = 60,
-		weaponid = weaponid
+		weaponid = weaponid,
+		phyShop = phyStmp
 	})
 
 end
@@ -27,6 +35,8 @@ end
 function Shop.draw()
 	for i , shop in ipairs(Shop.List) do
 		love.graphics.draw(shop.image, shop.x, shop.y)
+		shop.phyShop.b:setX(shop.x)	
+		shop.phyShop.b:setY(shop.y)
 	end
 end
 
