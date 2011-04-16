555b89f01f891574905bc132fe30f080b8c9d95d
Justina <j.mickonyte@gmail.com>
2011-04-16 15:28:15 +0300

    2 bars

---
+++ game/PlayerI.lua
@@ -0,0 +1,27 @@
+
+
+    PlayerI = {}
+function PlayerI.load()
+	PlayerI.image = love.graphics.newImage("images/PadariukasVio.png")
+	PlayerI.x = 0;
+	PlayerI.y = 0;
+end
+
+function PlayerI.update(dt)
+ if love.keyboard.isDown("left") then
+	PlayerI.x = PlayerI.x - 100 * dt
+    end
+    if love.keyboard.isDown("right") then
+	PlayerI.x = PlayerI.x + 100 * dt
+    end
+    if love.keyboard.isDown("up") then
+	PlayerI.y = PlayerI.y - 100 * dt
+    end
+    if love.keyboard.isDown("down") then
+	PlayerI.y = PlayerI.y + 100 * dt
+	end
+end
+
+function PlayerI.draw()
+	love.graphics.draw(PlayerI.image, PlayerI.x, PlayerI.y)
+end
