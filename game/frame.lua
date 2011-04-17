15daf4a84dd9710b4c1e7c4b905f0785fb65bbe3
Justina < j.mickonyte@gmail.com >
2011-04-17 08:09:50 +0300

    'loading' screen, frame, bars

---
+++ game/frame.lua
@@ -0,0 +1,20 @@
+frame = {}
+
+function frame.load()
+	frame.image = love.graphics.newImage(pre..'hood.png')
+	frame.eye = {
+				 [0] = love.graphics.newImage(pre..'akis/0.png'),
+				 [1] = love.graphics.newImage(pre..'akis/1.png'),
+				 [2] = love.graphics.newImage(pre..'akis/2.png'),
+				 [3] = love.graphics.newImage(pre..'akis/3.png')
+				}
+end
+
+--function frame.update()
+
+--end
+
+function frame.draw()
+	love.graphics.draw(frame.image)
+	
+end
